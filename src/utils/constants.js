f991f443e004339a6b0ffeb8a7582c2320ccc5e7
Corian2108 <jefferson.ruiz2108@gmail.com>
2022-03-14 21:42:26 -0500

    Últimos ajustes antes de la defensa

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_LOGIN = 'http://192.168.1.4:8000/api/login';
-export const API_GETAUTHUSER = 'http://192.168.1.4:8000/api/user';
-export const API_REGISTER = 'http://192.168.1.4:8000/api/register';
-export const API_POSTEVENT = 'http://192.168.1.4:8000/api/evento';
-export const API_POSTPLACE = 'http://192.168.1.4:8000/api/lugar';
+export const API_LOGIN = 'http://linkear.net:8000/api/login';
+export const API_GETAUTHUSER = 'http://linkear.net:8000/api/user';
+export const API_REGISTER = 'http://linkear.net:8000/api/register';
+export const API_POSTEVENT = 'http://linkear.net:8000/api/evento';
+export const API_POSTPLACE = 'http://linkear.net:8000/api/lugar';
